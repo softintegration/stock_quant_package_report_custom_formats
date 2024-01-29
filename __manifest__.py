7c36f177013eeb246983bf63735a54dcf9921f30
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-29 22:52:09 +0100

    [IMP] Package:Change the color of label columns to black

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.1',
+    'version': '1.0.1.2',
     'category': 'Inventory/Inventory',
     'demo': [],
     'depends': ['stock'],
