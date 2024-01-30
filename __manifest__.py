207d06e76daceee229cf014657609d9dda08eb3e
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-30 09:32:26 +0100

    [IMP] Package:Align labels at left

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.3',
+    'version': '1.0.1.4',
     'category': 'Inventory/Inventory',
     'demo': [],
     'depends': ['stock'],
