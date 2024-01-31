d5f7be449730f69cc3463efa90b3beecf9cd8ca8
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-31 09:33:42 +0100

    [FIX][IMP] Package:Fix the defaut paperformat «A4» for the default report «Package Barcode with Content»

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.4',
+    'version': '1.0.1.5',
     'category': 'Inventory/Inventory',
     'demo': [],
     'depends': ['stock'],
