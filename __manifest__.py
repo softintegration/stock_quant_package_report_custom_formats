5380311b61553bb4833939a1e6c42270484ad174
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-02-04 17:04:59 +0100

    [IMP] Package:Reduce the codebare table

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.5',
+    'version': '1.0.1.6',
     'category': 'Inventory/Inventory',
     'demo': [],
     'depends': ['stock'],
