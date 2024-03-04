0a88c14d3246f91668644ded69255947a681b968
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-03-04 17:18:38 +0100

    [IMP] Package:Add field «N° Order»

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.14',
+    'version': '1.0.1.15',
     'category': 'Inventory/Inventory',
     'demo': [],
     'depends': ['stock','web'],
