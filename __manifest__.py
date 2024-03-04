f99632ce5c4b5a2f47d8cc404c266e4cacd6290b
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-03-04 10:36:31 +0100

    [IMP] Package:Add Precision to the field «Quantity»

--- __manifest__.py
+++ __manifest__.py
@@ -10,12 +10,13 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.12',
+    'version': '1.0.1.13',
     'category': 'Inventory/Inventory',
     'demo': [],
     'depends': ['stock','web'],
     'data': [
         'data/report_paperformat_data.xml',
+        'data/stock_quant_package_report_custom_formats_data.xml',
         'report/report_package_barcode.xml',
         'report/stock_report_views.xml',
         'views/stock_quant_views.xml'
